05e1047e12068204b23e3c436a93423349302766
zohra-b <z.bellamine@gmail.com>
2025-06-17 10:37:44 +0200

    feat: playerService : created main methods

--- src/main/java/cat/itacademy/s5/_1/services/PlayerService.java
+++ src/main/java/cat/itacademy/s5/_1/services/PlayerService.java
@@ -1,16 +1,45 @@
 package cat.itacademy.s5._1.services;
 
+import cat.itacademy.s5._1.entities.Player;
 import cat.itacademy.s5._1.repositories.PlayerRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PlayerService {
+    private PlayerRepository playerRepo;
 
-    // save(T entity)	Sauvegarde ou met à jour un enregistrement	Mono<T>
-    // saveAll(Iterable<T> / Publisher<T>)	Sauvegarde plusieurs objets	Flux<T>
-    // findById(ID id)	Recherche un enregistrement par son ID	Mono<T>
-    // findAll()	Récupère tous les enregistrements	Flux<T>
+    @Autowired
+    public PlayerService(PlayerRepository playerRepo){
+        this.playerRepo = playerRepo;
+    }
+
+    public void createPlayer(Player newPlayer){
+        playerRepo.save(newPlayer);
+    }
+
+    public Flux<Player> findAll(){
+        return playerRepo.findAll();
+    }
+
+    public Mono<Player> findByID(UUID playerID){
+        return playerRepo.findById(playerID);
+    }
+
+    public Mono<UUID> getIdByPlayerEmail(String playerEmail){
+        return playerRepo.findByPlayerEmail(playerEmail)
+                    .map(Player::getPlayerID);
+    }
+
+    public void deletePlayerById(String playerEmail){
+        Mono<UUID> playerId = getIdByPlayerEmail(playerEmail);
+        playerRepo.deleteById(playerId);
+    }
     // existsById(ID id)	Vérifie si un enregistrement existe par ID	Mono<Boolean>
     // count()	Retourne le nombre total d’enregistrements	Mono<Long>
     // deleteById(ID id)	Supprime un enregistrement par ID	Mono<Void>
