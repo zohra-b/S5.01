d9faea1ab35a37e25a26b4b5b165ec87a91f4893
zohra-b <z.bellamine@gmail.com>
2025-06-17 11:28:11 +0200

    project Structure : added globalExceptionHandler + ErrorResponse

---
+++ src/main/java/cat/itacademy/s5/_1/exceptions/ErrorResponse.java
@@ -0,0 +1,35 @@
+package cat.itacademy.s5._1.exceptions;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+
+public class ErrorResponse {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd - MM - yyyy hh:mm:ss")
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private List<String> details;
+    private String path;
+
+
+    public ErrorResponse(HttpStatus status, String error, String message, List<String> details,String path ){
+        this.timestamp = LocalDateTime.now();
+        this.status = status.value();
+        this.error = error;
+        this.message = message;
+        this.details = details;
+        this.path = path;
+    }
+
+    public ErrorResponse(HttpStatus status, String message, String path){
+        this.timestamp = LocalDateTime.now();
+        this.status = status.value();
+        this.message = message;
+        this.path = path;
+    }
+}
---
+++ src/main/java/cat/itacademy/s5/_1/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package cat.itacademy.s5._1.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException illegalArgExc, WebRequest request){
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        String errorMsg = illegalArgExc.getMessage();
+        String path = request.getDescription(false).replace("uri=", "");
+
+        ErrorResponse errorResponse = new ErrorResponse(status, errorMsg, path);
+
+        return new ResponseEntity<>(errorResponse, status);
+    }
+
+}
