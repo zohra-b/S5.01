3ca6c3d22d490765d9f5811cc378348ef8c0e7ba
zohra-b <z.bellamine@gmail.com>
2025-06-20 14:15:33 +0200

    suppressed menus

--- src/main/java/cat/itacademy/s5/_1/Application.java
+++ src/main/java/cat/itacademy/s5/_1/Application.java
@@ -1,39 +1,14 @@
 package cat.itacademy.s5._1;
 
-import cat.itacademy.s5._1.menus.MainMenu;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args)
-				.getBean(MainMenu.class)
-				.start()
-				.subscribe( // <-- SEUL subscribe() de l'application !
-						null,
-						error -> System.err.println("Erreur : " + error),
-						() -> System.out.println("Merci d'avoir joué !")
-				);
+		SpringApplication.run(Application.class, args);
+			System.out.println("WELCOME TO OUR BLACK JACK GAME");
 	}
-
 }
-//public static void main(String[] args) {
-//	ConfigurableApplicationContext context =
-//			SpringApplication.run(Application.class, args);
-//
-//	MainMenu menu = context.getBean(MainMenu.class);
-//
-//	// Souscription explicite avec gestion des erreurs
-//	menu.start().subscribe(
-//			null,
-//			error -> System.err.println("Erreur : " + error),
-//			() -> System.out.println("Merci d'avoir joué !")
-//	);
-//
-//	// Optionnel : Arrêter proprement Spring après la fin du menu
-//	context.close();
-//}
--- src/main/java/cat/itacademy/s5/_1/menus/GameMenu.java
+++
@@ -1,8 +0,0 @@
-package cat.itacademy.s5._1.menus;
-
-public class GameMenu {
-    // while (input != "N" )this is your hand,
-    //  1 - hit  --> drawCard()
-    // 2 - stand --> calculateWinner()
-
-}
