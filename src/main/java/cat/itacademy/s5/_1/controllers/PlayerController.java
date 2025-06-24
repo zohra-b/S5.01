a1f0bdb211c5e88b096824b2d5b033ef9c27c4a3
zohra-b <z.bellamine@gmail.com>
2025-06-24 14:52:09 +0200

    hotfix : changed @mapping of getPlayerbyEmail() and getPlayerbyId()

--- src/main/java/cat/itacademy/s5/_1/controllers/PlayerController.java
+++ src/main/java/cat/itacademy/s5/_1/controllers/PlayerController.java
@@ -2,14 +2,11 @@ package cat.itacademy.s5._1.controllers;
 
 import cat.itacademy.s5._1.dtos.PlayerDTO;
 import cat.itacademy.s5._1.dtos.PlayerRegistrationRequestDTO;
-import cat.itacademy.s5._1.exceptions.ErrorResponse;
 import cat.itacademy.s5._1.exceptions.PlayerNotFoundException;
 import cat.itacademy.s5._1.services.PlayerService;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.Objects;
-import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -33,14 +30,14 @@ public class PlayerController {
                 .map(PlayerDTO::fromEntity);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/playerById/{id}")
     public Mono<ResponseEntity<Object>> getPlayerByID (@PathVariable String id){
         return playerService.findByID(id)
                 .map(playerDTO -> ResponseEntity.ok().body((Object)playerDTO))
                 .onErrorResume(PlayerNotFoundException.class, e -> Mono.just(ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage())));
     }
 
-    @GetMapping("/{email}")
+    @GetMapping("/playerByEmail/{email}")
     public Mono<ResponseEntity<Object>> getPlayerbyEmail(@PathVariable String email) {
 
         return playerService.findByPlayerEmail(email)
@@ -77,7 +74,7 @@ public class PlayerController {
     @PutMapping("/{id}/updateName")
     public Mono<ResponseEntity<Object>> updatePlayerName(@PathVariable String id, @RequestParam String newName){
         return playerService.updatePlayerName(id, newName)
-                .map(playerDTO -> ResponseEntity.status(HttpStatus.OK).body((Object)"Score updated successfully"))
+                .map(playerDTO -> ResponseEntity.status(HttpStatus.OK).body((Object)"Name updated successfully"))
                 .onErrorResume(PlayerNotFoundException.class, e -> Mono.just(ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage())));
     }
 
